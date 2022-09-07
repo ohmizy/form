6a3f262acecb6b47b15e204d0285228ab4a8ead3
ohmizy <omarjabr49@yahoo.com>
2022-09-06 17:06:29 -0700

    Initial Commit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,32 +1,75 @@
 import React, { useState } from "react"
 
 const Form = () => {
-  const [name, setName] = useState ("");
+  const [firstName, setFirstName] = useState ("");
+  const [firstNameError, setFirstNameError] = useState(null);
+  const [lastName, setLastName] = useState ("");
+  const [LastNameError, setLastNameError] = useState(null);
   const [email, setEmail] = useState ("");
-  const [password, setPassword] = useState ("")
+  const [emailError, setEmailError] = useState(null);
+  const [password, setPassword] = useState ("");
+  const [passwordError, setPasswordError] = useState(null);
+  const [confirmPassword, setConfirmPassword] = useState ("");
+  const [confirmPasswordError, setConfirmPasswordError] = useState(null);
+
+  const validateFirstName = () => {
+    if (firstName.length < 3 ) {
+      setFirstNameError("Name must be longer than 3 characters.")
+    } else {
+      setFirstNameError(null);
+    }
+  }
+
+  const validateEmail = () => {
+    if (email.length < 3 ) {
+      setEmailError("Email must be longer than 3 characters.")
+    } else {
+      setEmailError(null);
+    }
+  }
+
+  const validatePassword = () => {
+    if (password.length < 3 ) {
+      setPasswordError("Password must be longer than 3 characters.")
+    } else {
+      setPasswordError(null);
+    }
+  }
+  const validateConfirmPassword = () => {
+    if (password.length < 3 ) {
+      setPasswordError("Password must be longer than 3 characters.")
+    } else {
+      setPasswordError(null);
+    }
+  }
   
   return (
     <form className="mt-3">
       <div className="mb-3">
-        <label htmlFor="name" className="form-label">First Name</label>
+        <label htmlFor="firstName" className="form-label">First Name</label>
         <input
           type="text"
-          name="name"
-          id="name"
+          name="firstName"
+          id="firstName"
           className="form-control"
-          value = {name}
-          onChange = { (e) => { setName(e.target.value) } }
+          value = {firstName}
+          onChange = { (e) => { setFirstName(e.target.value) } }
+          onBlur={validateFirstName}
         />
+        {
+          firstNameError &&
+          <span className="form-text text-danger">{ firstNameError }</span>
+        }
       </div>
       <div className="mb-3">
-        <label htmlFor="name" className="form-label">Last Name</label>
+        <label htmlFor="lastName" className="form-label">Last Name</label>
         <input
           type="text"
-          name="name"
-          id="name"
+          name="lastName"
+          id="lastName"
           className="form-control"
-          value = {name}
-          onChange = { (e) => { setName(e.target.value) } }
+          value = {lastName}
+          onChange = { (e) => { setLastName(e.target.value) } }
         />
       </div>
       <div className="mb-3">
@@ -38,7 +81,12 @@ const Form = () => {
           className="form-control"
           value={email}
           onChange = {event => setEmail(event.target.value)}
+          onBlur={validateEmail}
         />
+        {
+          emailError &&
+          <span className="form-text text-danger">{ emailError }</span>
+        }
       </div>
       <div className="mb-3">
         <label htmlFor="password" className="form-label">Password</label>
@@ -52,15 +100,20 @@ const Form = () => {
         />
       </div>
       <div className="mb-3">
-        <label htmlFor="password" className="form-label">Confirm Password</label>
+        <label htmlFor="confirmPassword" className="form-label">Confirm Password</label>
         <input
-          type="password"
-          name="password"
-          id="password"
+          type="confirmPassword"
+          name="confirmPassword"
+          id="confirmPassword"
           className="form-control"
-          value={password}
-          onChange = {event => setPassword(event.target.value)}
-        />
+          value={confirmPassword}
+          onChange = {event => setConfirmPassword(event.target.value)}
+          onBlur={validateConfirmPassword}
+          />
+          {
+            confirmPasswordError &&
+            <span className="form-text text-danger">{ confirmPasswordError }</span>
+          }
       </div>
     </form>
   )
